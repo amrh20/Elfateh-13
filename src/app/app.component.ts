bb4323f69e043a27ad6439ca5de77192bb016bf0
amrh20 <amrhamdy058@gmail.com>
2025-08-10 00:25:53 +0300

    pwe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { InstallPromptComponent } from './components/install-prompt/install-prompt.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, InstallPromptComponent],
   template: `
     <app-header></app-header>
     <main>
       <router-outlet></router-outlet>
     </main>
+    <app-install-prompt></app-install-prompt>
   `,
   styles: []
 })
